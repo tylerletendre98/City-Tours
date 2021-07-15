512f28a9d1b711ac00740565d54a522c2453875c
T U <tylerletendre98@gmail.com>
2021-07-15 18:01:26 -0400

    added in some dummy data for the tourdata

---
+++ src/tourData.js
@@ -0,0 +1,31 @@
+export const tourData = 
+    [
+        {
+            id:1,
+            city: "Boston",
+            state:"Massachuesetts",
+            img: "https://th.bing.com/th/id/OIP.hJwU5bJP08TGkaN-dY9lKAHaE5?w=233&h=180&c=7&o=5&dpr=1.25&pid=1.7",
+
+        },
+        {
+            id:2,
+            city: "Las Vegas",
+            state:"Nevada",
+            img: "https://th.bing.com/th/id/OIP.hJwU5bJP08TGkaN-dY9lKAHaE5?w=233&h=180&c=7&o=5&dpr=1.25&pid=1.7",
+
+        },
+        {
+            id:3,
+            city: "New York",
+            state:"New York",
+            img: "https://th.bing.com/th/id/OIP.hJwU5bJP08TGkaN-dY9lKAHaE5?w=233&h=180&c=7&o=5&dpr=1.25&pid=1.7",
+
+        },
+        {
+            id:4,
+            city: "San Deigo",
+            state:"California",
+            img: "https://th.bing.com/th/id/OIP.hJwU5bJP08TGkaN-dY9lKAHaE5?w=233&h=180&c=7&o=5&dpr=1.25&pid=1.7",
+
+        }
+    ]
